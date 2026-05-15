eab7cc9e413ba020c8a7b563a008d1022d780a02
campusbuddy-wat <campusbuddywat@gmail.com>
2026-05-15 11:37:40 +0600

    Fix: Force HTTPS in production to resolve 405 error on admin login

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
+
         View::composer(
             'includes.topbar',
             \App\View\Composers\TopbarComposer::class
